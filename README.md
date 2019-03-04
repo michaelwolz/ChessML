e66ceeb846b86692d6b9a47e2f2c32d49d4eceaa
Michael Wolz <michaelwolz93@gmail.com>
2019-03-04 19:33:40 +0100

    Added readme

---
+++ README.md
@@ -0,0 +1,2 @@
+Thanks to https://medium.com/@daylenyang/building-chess-id-99afa57326cd
+and https://medium.com/@neshpatel/solving-sudoku-part-ii-9a7019d196a2
--- chessboard_processor.py
+++ chessboard_processor.py
@@ -9,13 +9,6 @@ import scipy.spatial as spatial
 import scipy.cluster as clstr
 
 
-
-"""
-Thanks to https://medium.com/@daylenyang/building-chess-id-99afa57326cd
-and https://medium.com/@neshpatel/solving-sudoku-part-ii-9a7019d196a2
-"""
-
-
 def canny(img):
     # Maybe I'll add some auto thresholding here
     edges = cv2.Canny(img, 100, 200)
